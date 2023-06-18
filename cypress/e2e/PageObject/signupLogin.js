035c0525e6e4550a47e5d92f9d376b92436cceac
marcelodanieldm <danielmarcelo83@gmail.com>
2023-06-17 22:57:30 -0300

    first commit - signupLogin new banch - idetified signup login tcs

---
+++ cypress/e2e/PageObject/signupLogin.js
@@ -0,0 +1,25 @@
+
+//Locators goes here
+
+
+class signupLogin{
+
+    static registerUser(){
+
+    }
+
+    static loginUserOK(){
+
+    }
+
+    static loginWrongEmail(){
+
+    }
+
+    static logOut(){
+
+    }
+
+}
+
+export default signupLogin
---
+++ cypress/e2e/Tests/signupLogin.cy.js
@@ -0,0 +1,29 @@
+
+
+describe('Login and Sign up test cases', function(){
+
+    it('Register user', function(){
+
+    } )
+
+    it('Login user with correct e-mail and password', function(){
+        
+    } )
+
+    it('Login user wth incorrect e-mail and password', function(){
+        
+    } )
+
+    it('Logout user', function(){
+        
+    } )
+
+    it('Register user with existing e-mail', function(){
+        
+    } )
+
+
+
+
+}
+)
